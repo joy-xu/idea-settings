d9eab989c604fe331aa8e6cdb37716d0bd673bef
xuliang.sxl <xuliang.sxl@alibaba-inc.com>
2019-05-21 16:42:07 +0800

    IU-2019.1 <joy@ali-6c96cfd99803.local Create p3c.xml, project.default.xml, Enum.java, Interface.java, Default.xml, ignore.xml, find.xml, code.style.schemes.xml, KeyPromoterXSettings.xml, path.macros.xml, Darcula copy.icls, mavenVersion.xml, javarebel.xml, gutter.xml, Default.xml, duplocatorSettings.xml, editor.codeinsight.xml, grepConsole.xml, keymap.xml, editor.xml, markdown_navigator.shared.xml, debugger.xml, updates.xml, baseRefactoring.xml, vim_settings.xml, ide.general.xml, other.xml, rainbow_brackets.xml, lombok-plugin.xml, git.xml, postfixTemplates.xml, terminal.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, projectView.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, github.xml, templates.xml, AnnotationType.java, output.xml, KeyPromoterXStatistic.xml, Class.java, jdk.table.xml, smartfox_info.xml, mavenRunHelper.xml, laf.xml, Mac OS X 10_5_ copy.xml, vcs.xml, tala_settings.xml, Alibaba-CodeStyle.xml, databaseSettings.xml, toStringTemplates.xml, usageView.xml, Mac OS X copy.xml

---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * ${DESCRIPTION}
+ *
+ * @author ${USER}  on ${DATE} 
+ */
+public enum ${NAME} {
+}
